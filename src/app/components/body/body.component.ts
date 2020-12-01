9bc8678db6448038900bce6e3468d010114f90c0
Hugo Muñoz <66710447+munozphugo@users.noreply.github.com>
2020-11-30 18:34:58 -0600

    Terminado

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+
+  frase: any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad.',
+    autor: 'Ben Parker'
+  };
+
+  mostrar = true;
+
+  personajes: string[] = ['Spiderman', 'venom', 'Dr. Octopus'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
